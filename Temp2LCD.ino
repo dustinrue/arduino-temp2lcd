9b9d184b2c5c96182c0ac087f6a8c5bfaa3a48ec
dustinrue <ruedu@dustinrue.com>
2016-10-27 08:13:20 -0500

    timer based, gets outside data from serial

--- Temp2LCD.ino
+++ Temp2LCD.ino
@@ -1,12 +1,14 @@
 #include <DHT.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <SimpleTimer.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 DHT dht(DHTPIN, DHTTYPE);
+SimpleTimer timer;
 
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
@@ -19,33 +21,39 @@ void setup() {
   pinMode(LED_BUILTIN, HIGH);
   lcd.begin(20,4);
   lcd.setCursor(0,0);
-  lcd.print("Temperature: ");
+  lcd.print("Office: ");
+  lcd.setCursor(0,1);
+  lcd.print("Outside: ");
   inputString.reserve(200);
+
+  timer.setInterval(120000, getTemperature);
+  timer.setInterval(300000, getOutsideTemperature);
+  getTemperature();
+  getOutsideTemperature();
 }
 
 void loop() {
-  if (stringComplete) {
-    // Arduino doesn't do strings in switch case statements
-    if (inputString == "temperature") {
-      getTemperature();
-    }
-    else if (inputString == "humidity") {
-      getHumidity();
-    }
-    else {
-      // don't do anything
-    }
+  timer.run();
+  if (stringComplete) {   
     
-    clearLine(1);
-    lcd.print(inputString);
+    lcd.setCursor(9,1);
+    lcd.print("           ");
+    clearLine(3);
+    
+    int colon = inputString.indexOf(':');
+    String outsideTemp = inputString.substring(0,colon);
+    String conditions = inputString.substring(colon + 1, inputString.length());
+
+    lcd.setCursor(9,1);
+    lcd.print(outsideTemp);
+    lcd.setCursor(0,3);
+    lcd.print(conditions);
     // clear the string:
     inputString = "";
     stringComplete = false;
-
-    // the DHT11 needs a couple of seconds to settle
-    // after being polled
-    delay(2000);
+    
   }
+
 }
 
 void serialEvent() {
@@ -69,23 +77,13 @@ void getTemperature() {
   String output = "";
   output.concat(f);
   output.concat('\n');
-  Serial.write(output.c_str());
   digitalWrite(LED_BUILTIN, LOW);
-  lcd.setCursor(13,0);
+  lcd.setCursor(8,0);
   lcd.print(f);
 }
 
-void getHumidity() {
-  digitalWrite(LED_BUILTIN,HIGH);
-  //float f = dht.readTemperature(true);
-  float f = dht.readHumidity(true);
-  String output = "";
-  output.concat(f);
-  output.concat('\n');
-  Serial.write(output.c_str());
-  digitalWrite(LED_BUILTIN, LOW);
-  lcd.setCursor(13,0);
-  lcd.print(f);
+void getOutsideTemperature() {
+  Serial.write("update\n");
 }
 
 void clearLine(int row) {
